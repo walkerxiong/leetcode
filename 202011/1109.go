5bf82edd268e7a41738b8f265aaafe8db6056819
gbabyX <showphp@foxmail.com>
2020-11-11 13:50:56 +0800

    add heap data struct

--- 202011/1109.go
+++ 202011/1109.go
@@ -1,45 +1,22 @@
 package main
 
 import (
-	"container/heap"
-	"fmt"
+	"github.com/gbabyX/leetcode/util"
 )
-
-type IntHeap []int
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(int))
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
 
 func kClosest(points [][]int, K int) [][]int {
 	var (
 		res [][]int
 		m   = make(map[int][]int) // distance => seq
-		h   = &IntHeap{}
+		h   = util.NewIntHeap(func(i, j int) bool { return i <= j }, K)
 	)
-	heap.Init(h)
 	for seq, arr := range points {
 		distance := arr[0]*arr[0] + arr[1]*arr[1]
 		m[distance] = append(m[distance], seq)
-		heap.Push(h, distance)
+		h.Push(distance)
 	}
-	fmt.Println(m)
 	for i := 0; i < K; {
-		v := heap.Pop(h).(int)
+		v := h.Pop()
 		val := m[v]
 		if val != nil {
 			for _, s := range val {
@@ -50,7 +27,6 @@ func kClosest(points [][]int, K int) [][]int {
 				i++
 			}
 		}
-
 	}
 
 	return res
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gbabyX/leetcode
+
+go 1.15
