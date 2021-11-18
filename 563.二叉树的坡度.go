b26def5dc00d9891c7b1dd372f9e4987a1e570e0
walkerxiong <annoymous.walker@foxmail.com>
2021-11-18 23:57:29 +0800

    二叉树坡度，标准答案

---
+++ 563.二叉树的坡度.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=563 lang=golang
+ *
+ * [563] 二叉树的坡度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTilt(root *TreeNode) (ans int) {
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		sumLeft := dfs(node.Left)
+		sumRight := dfs(node.Right)
+		ans += abs(sumLeft - sumRight)
+		return sumLeft + sumRight + node.Val
+	}
+	dfs(root)
+	return
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// @lc code=end
+
